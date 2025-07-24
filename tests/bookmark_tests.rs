0b7939e58308286bb2825c5540efbb48b766efdc
Jesse Wattenbarger <jwatt@broken.watch>
2025-07-24 08:45:20 -0400

    Start writing tests

---
+++ tests/bookmark_tests.rs
@@ -0,0 +1,28 @@
+#[cfg(test)]
+mod tests {
+    use super;:*;
+    use tempfile::{TempDir, NamedTempFile};
+    use std::fs;
+    use std::io::{BufWriter, Write};
+    use std::path::Path;
+    use std::env;
+
+    /// Helper function to create a temp file with known bookmark data
+    fn create_temp_bookmarks_file(path: &Path, bookmarks: &[Bookmark]) -> std::io::Result<()> {
+	let mut file = std::fs::File::create(path)?;
+	for bookmark in bookmarks {
+	    writeln!(file, "{}{}", bookmark.name, DELIM)?;
+	    writeln!(file, "{}", bookmark.path)?;
+	}
+	Ok(())
+    }
+
+    /// Test bookmark_file_path
+    #[test]
+    fn test_bookmark_file_path() {
+	// Set environment variable for testing.
+	env::set_var("WDC_BOOKMARK_FILE", "/test/path");
+	let path = bookmark_file_path().unwrap();
+	assert_eq!(path, "/test/pathh");
+    }
+}
