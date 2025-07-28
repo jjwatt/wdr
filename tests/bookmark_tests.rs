5f0aa3d02a48a7233f8a60a365a3417c18f6d937
Jesse Wattenbarger <jwatt@broken.watch>
2025-07-27 21:43:23 -0400

    Messing around with a test wrapper and serial_test

--- tests/bookmark_tests.rs
+++ tests/bookmark_tests.rs
@@ -1,18 +1,35 @@
 use wdr::*;
+use std::path::{Path, PathBuf};
 use tempfile::{TempDir, NamedTempFile};
 use std::fs::File;
 use std::io::{BufWriter, Write};
-use std::path::Path;
 use std::env;
+use serial_test::serial;
 
-/// Helper function to create a temp file with known bookmark data
-fn create_temp_bookmarks_file(path: &Path, bookmarks: &[Bookmark]) -> std::io::Result<()> {
-    let mut file = std::fs::File::create(path)?;
-    for bookmark in bookmarks {
-	writeln!(file, "{}{}", bookmark.name, DELIM)?;
-	writeln!(file, "{}", bookmark.path)?;
+fn with_test_env<F>(test_body: F)
+where
+    // The test body is a closure that gets the path to the temp file.
+    F: FnOnce(PathBuf),
+{
+    let original_val = env::var(BM_ENV);
+    let temp_dir = TempDir::new().unwrap();
+    let file_path = temp_dir.path().join(".test_bookmarks");
+    // Set the env var just for this test.
+    unsafe { std::env::set_var(BM_ENV, &file_path); }
+    test_body(file_path);
+    match original_val {
+	Ok(val) => unsafe { std::env::set_var(BM_ENV, val) },
+	Err(_) => unsafe  { std::env::remove_var(BM_ENV) },
     }
-    Ok(())
+}
+
+#[serial]
+#[test]
+fn test_bookmark_file_path_env_override() {
+    with_test_env(|custom_path| {
+	let path = bookmark_file_path().unwrap();
+	assert_eq!(path, custom_path);
+    });
 }
 
 /// Test bookmark_file_path
